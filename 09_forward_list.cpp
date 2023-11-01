84af6472e9c31ac049107560c2f886e7866aa38e
jonyLon <masterlordgoker@gmail.com>
2023-11-01 19:28:30 +0200

    Add project files.

---
+++ 09_forward_list.cpp
@@ -0,0 +1,22 @@
+﻿#include "FList.h"
+
+int main()
+{
+    FList<int> list;
+
+    list.addHead(25);
+    list.addHead(22);
+    list.addTail(55);
+    list.addTail(51);
+    list.print();
+    FList<int> list2;
+    list2.addHead(25);
+    list2.addHead(28);
+    list2.addHead(22);
+    list2.addTail(55);
+    list2.addTail(220);
+    list2.print();
+
+    FList<int> test = list2 - list;
+    test.print();
+}
---
+++ FList.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+
+template <typename T>
+struct Node {
+	T data;
+	Node<T>* next;
+	Node(const T& data, Node<T>* next = nullptr) : data{ data }, next{ next }
+	{
+
+	}
+};
+
+
+
+template <typename T>
+class FList
+{
+public:
+	FList() = default;
+	~FList() {
+		while (!isEmpty()) {
+			removeHead();
+		}
+	}
+	void print() const {
+		auto* tmp = head;
+		while (tmp != nullptr)
+		{
+			cout << tmp->data << "\t";
+			tmp = tmp->next;
+		}
+		cout << endl;
+	};
+	bool isEmpty() const {
+		return head == nullptr;
+	};
+	void removeHead() {
+		auto* tmp = head;
+		head = head->next;
+		if (isEmpty()) {
+			tail = nullptr;
+			return;
+		}
+		delete tmp;
+		--size;
+	}
+	void addHead(const T& data) {
+		Node<T>* tmp = new Node<T>(data, head);
+		if (isEmpty())
+		{
+			tail = tmp;
+		}
+		head = tmp;
+		++size;
+	}
+	void clear() {
+		while (!isEmpty()) {
+			removeHead();
+		}
+	}
+	void addTail(const T& data) {
+		Node<T>* tmp = new Node<T>(data);
+		if (isEmpty())
+		{
+			head = tail = tmp;
+		}
+		else {
+			tail->next = tmp;
+			tail = tmp;
+		}
+		++size;
+	}
+
+	void operator+=(const FList& other) {
+		auto* tmp = other.head;
+		while (tmp != nullptr)
+		{
+			addTail(tmp->data);
+			tmp = tmp->next;
+		}
+	}
+	FList operator-(const FList& other) const{
+		FList newList;
+		T* items = new T[size];
+		int counter = 0;
+		auto* tmp = head;
+		auto* ptr = other.head;
+		while (tmp != nullptr)
+		{
+			items[counter++] = tmp->data;
+			tmp = tmp->next;
+		}
+		while (ptr != nullptr)
+		{
+			for (size_t i = 0; i < counter; i++)
+			{
+				if (ptr->data == items[i]) {
+					newList.addTail(items[i]);
+					break;
+				};
+			}
+			ptr = ptr->next;
+		}
+
+		return newList;
+	}
+	
+private:
+	Node <T>* head = nullptr;
+	Node <T>* tail = nullptr;
+	size_t size = 0;
+};
+
