2f5fa3e574d329838de844b109b7c8abfd4b239b
jonyLon <masterlordgoker@gmail.com>
2023-11-01 19:34:50 +0200

    testing

--- 09_forward_list.cpp
+++ 09_forward_list.cpp
@@ -3,12 +3,18 @@
 int main()
 {
     FList<int> list;
-
     list.addHead(25);
     list.addHead(22);
     list.addTail(55);
     list.addTail(51);
     list.print();
+    FList<int> list3;
+    list3.addHead(12);
+    list3.addTail(45);
+    list3.addTail(56);
+    list3.print();
+    list3 += list;
+    list3.print();
     FList<int> list2;
     list2.addHead(25);
     list2.addHead(28);
